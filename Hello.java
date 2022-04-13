483fad5a9c1c59f472f30e4c9aa1348fc0165e33
shanto-bepary <shantobepary575@gmail.com>
2022-04-13 23:19:57 +0600

    Add a new methood colled display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class hello{
     public static void main(String[]){
         system.out.printin("VS code is Awsome...");
+
+        greating('Shanto Bepary');
+    }
+    public static void greating(String name){
+        System.out.printin("Welcome"=name);
     }
 }
