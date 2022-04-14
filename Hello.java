57b8ef9f3ebd219fc51af59e78ce23b05e2ece44
shanto-bepary <shantobepary575@gmail.com>
2022-04-14 10:09:23 +0600

    remove all methood and creat a new methood named message

--- Hello.java
+++ Hello.java
@@ -2,12 +2,9 @@ public class hello{
     public static void main(String[]){
         system.out.printin("VS code is Awsome...");
 
-        display();
+     
     }
-    public static void display(){
-        system.out.printin("Hello Hello);
-    }
-    public static int sqr(int x){
-        return x x;
+    public static void message(){
+        system.out.printin("Hoorray!!!! Now WE are able to contribute");
     }
 }
